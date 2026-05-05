57cbf3f730c186e10a520cbd75219c978ecfa54a
dhanidds <tonodds@gmail.com>
2026-05-05 13:32:50 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,26 +1,20 @@
+# kalkulator.py
 def tambah(a, b):
-  """
-  Fungsi ini menerima dua angka dan mengembalikan hasil penjumlahannya.
-  """
-  return a + b
-
+  """Fungsi ini menerima dua angka dan mengembalikan hasil penjumlahannya."""
+  # TODO: Hapus 'pass' dan tulis kode Anda di sini
+  pass
+  
 def kurang(a, b):
-  """
-  Fungasi ini menerima dua angka dan mengembalikan hasil pengurangannya.
-  """
-  return a - b
+  """Fungasi ini menerima dua angka dan mengembalikan hasil pengurangannya."""
+  # TODO: Hapus 'pass' dan tulis kode Anda di sini
+  pass
 
 def kali(a, b):
-  """
-  Fungsi ini menerima dua angka dan mengembalikan hasil perkaliannya.
-  """
-  return a * b
+  """Fungsi ini menerima dua angka dan mengembalikan hasil perkaliannya."""
+  # TODO: Hapus 'pass' dan tulis kode Anda di sini
+  pass
 
 def bagi(a, b):
-  """
-  Fungsi ini menerima dua angka dan mengembalikan hasil pembagiannya.
-  Jika pembaginya adalah 0, kembalikan string "Error: Pembagian dengan nol".
-  """
-  if b == 0:
-      return "Error: Pembagian dengan nol"
-  return a / b
+  """Fungsi ini menerima dua angka dan mengembalikan hasil pembagiannya. Jika pembaginya adalah 0, kembalikan string "Error: Pembagian dengan nol"."""
+  # TODO: Hapus 'pass' dan tulis kode Anda di sini
+  pass
