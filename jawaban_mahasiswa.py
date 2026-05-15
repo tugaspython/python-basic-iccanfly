6d4ba7db2bfd9b1f7ad0477a3684d7750df26042
iccanfly <iccanfly@gmail.com>
2026-05-15 17:27:10 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,1 +1,26 @@
-# JAWAB DISINI!
+def validasi_password(password):
+    if len(password) < 8 or len(password) > 20:
+        return False
+
+    has_upper = False
+    has_digit = False
+    has_symbol = False
+    has_space = False
+
+    for char in password:
+        if char.isupper():
+            has_upper = True
+        elif char.isdigit():
+            has_digit = True
+        elif char in "!@#$%^&*":
+            has_symbol = True
+        elif char == " ":
+            has_space = True
+
+    if has_space:
+        return False
+
+    if has_upper and has_digit and has_symbol:
+        return True
+    else:
+        return False
